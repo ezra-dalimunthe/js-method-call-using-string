0be02f50b17e7eea7e146e3a6e93bc50149aae07
Ezra Dalimunthe <ezra.dalimunthe@gmail.com>
2022-10-12 00:15:25 +0700

    remove foo from this file

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-import foo from "./foo.js";
 import Caller from "./caller.js";
-function helloBar() {
-  var f = new foo();
-  const method = "bar";
-  return f[method]();
-}
+
 window.addEventListener(
   "load",
   function () {
